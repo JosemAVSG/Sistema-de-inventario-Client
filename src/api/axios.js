537366ccce348f0000cf008c3ac7d55e2eb79f96
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-04-19 11:53:09 -0500

    changing api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-//  const url = "https://sistema-de-inventario-backend.onrender.com/api";
+ const url = "https://sistema-de-inventario-backend.onrender.com/api";
 
 const instance = axios.create({
-    baseURL:'http://localhost:3000/api',
-    // baseURL:url,
+    // baseURL:'http://localhost:3000/api',
+    baseURL:url,
     withCredentials:true,
 })
 
